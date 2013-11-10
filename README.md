ed3bde264615a54a03da26a777676542cde2b7c9
Fèlix Galindo Allué <felix@flx.cat>
2013-11-10 14:56:11 +0100

    Better documentation and config issues

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 jekyll-liquid-latex-plugin
 ==========================
 
-Jekyll plugin that defines a useful Liquid Tag for rendering blocks of LaTeX code inside a post
+[Jekyll](http://Jekyllrb.com/) plugin that defines a useful Liquid Tag for rendering blocks of [LaTeX](http://en.wikipedia.org/wiki/LaTeX‎) code inside a post
 
 Installation
 ============
 
 Just copy the `liquid_latex.rb` to the `_plugins` folder.
+
+Requirements
+============
+
+Obviously you must have a LaTeX installation of some kind (like [TeXLive](http://www.tug.org/texlive/)) and some additional software capable of transforming a EPS file to its corresponding PNG equivalent (like [ImageMagick](http://www.imagemagick.org/)).
 
 Configuration
 =============
@@ -18,11 +23,77 @@ There are several configuration parameters that you can define in your `_config.
     # ...
 
     # Liquid-LaTeX plugin
-    latex_debug_mode: false
-    latex_density: 300
-    latex_output_directory: /res/latex
-    latex_latex_cmd: "latex -interaction=nonstopmode $1 &> /dev/null"
-    latex_dvips_cmd: "dvips -E $1 -o $2 &> /dev/null"
-    latex_convert_cmd: "convert -density 300 $1 $2 &> /dev/null"
-    latex_temp_filename: "latex_temp"
+    liquid_latex:
+      debug: false
+      density: 300
+      usepackages: pst-all,pst-3dplot
+      output_directory: /res/latex
+      latex_cmd: "latex -interaction=nonstopmode $texfile &> /dev/null"
+      dvips_cmd: "dvips -E $dvifile -o $epsfile &> /dev/null"
+      convert_cmd: "convert -density $density $epsfile $pngfile &> /dev/null"
+      temp_filename: "latex_temp"
+
+An explanation of those parameters follows:
+
+*   **`debug`** (`true`|`false`): Activates the debug mode with which you can see the compilation commands that are executed during build. Default value: `false`,
+
+*   **`density`** (numeric): Density for the conversion of PostScript (EPS) to PNG. Default value:`300`,
+
+*   **`usepackages`** (list of comma-separated strings): Name of the packages that will be passed globally to each block of LaTeX code. They will be added individually to their corresponding `\usepackage{...}` lines in the temporary $\LaTeX$ file. Default value: empty string (no packages),
+
+*   **`output_directory`** (web path): Path in which the generated PNG will be placed. Default value: `/latex`,
+
+*   **`temp_filename`** (string): Name of the temporary file that will be generated for the compilation process. Default value: "latex_temp"
+
+*   **`latex_cmd`** (string): Command line to execute for the `.tex` to `.div` conversion. Default value:
+
+        latex -interaction=nonstopmode $texfile &> /dev/null
+
+*   **`dvips_cmd`** (string): Command line to execute for the `.dvi` to `.eps` conversion. Default value:
+
+        dvips -E $dvifile -o $epsfile &> /dev/null
+
+*   **`convert_cmd`** (string): Command line to execute for the `.eps` to `.png` conversion. Default value:
+
+        convert -density $density $epsfile $pngfile &> /dev/null
+
+In these last three parameters you can use the following variables:
+
+*   **`$texfile`**: Name of the LaTeX temporary file
+*   **`$dvifile`**: Name of the DVI temporary file
+*   **`$epsfile`**: Name of the EPS temporary file
+*   **`$pngfile`**: Name of the generated PNG file
+*   **`$density`**: Density value that will be used by the conversion
+
+This variables makes you able to choose which software you use for the whole conversion process. The sample shown above, as well as the default parameters, where defined for use with TeXLive and ImageMagick.
+
+If your don't use a certain parameter, it will take the default value. If you are happy with all the default values you can omit the `liquid-latex` section in your `_config.xml`.
+
+Sample usage
+============
+
+You can type the following block of LaTeX inside one of your posts:
+
+{% latex density=72 usepackages=sudoku %}
+\begin{sudoku}
+| |2| | |3| |9| |7|.
+| |1| | | | | | | |.
+|4| |7| | | |2| |8|.
+| | |5|2| | | |9| |.
+| | | |1|8| |7| | |.
+| |4| | | |3| | | |.
+| | | | |6| | |7|1|.
+| |7| | | | | | | |.
+|9| |3| |2| |6| |5|.
+\end{sudoku}
+{% endlatex %}
+
+And you will get that sudoku rendered by the `sudoku` package. Don't forget to install the `sudoku` package in your LaTeX installation before trying to build your site. If not, the build process will stop and you'll get the original LaTeX code inside your post as a block of code. That's how this plugin will behave when your LaTeX code contains errors.
+
+In this case, you would probably need to install the `sudoku` package by executing:
+
+    sudo tlmgr install sudoku
+
+More information
+================
 
--- liquid_latex.rb
+++ liquid_latex.rb
@@ -6,134 +6,127 @@ module Jekyll
     class LatexBlock < Liquid::Block
       include Liquid::StandardFilters
 
-      @@debugmode = false
-      @@default_latex_density           = "300"
-      @@default_latex_latex_cmd         = "latex -interaction=nonstopmode $1 &> /dev/null"
-      @@default_latex_dvips_cmd         = "dvips -E $1 -o $2 &> /dev/null"
-      @@default_latex_convert_cmd       = "convert -density 300 $1 $2 &> /dev/null"
-      @@default_latex_output_directory  = "/latex"
-      @@default_latex_temp_filename     = "latex_temp"
+      @@globals = {
+        "debug" => false,
+        "density" => "300",
+        "usepackages" => "",
+        "latex_cmd" => "latex -interaction=nonstopmode $texfile &> /dev/null",
+        "dvips_cmd" => "dvips -E $dvifile -o $epsfile &> /dev/null",
+        "convert_cmd" => "convert -density $density $epsfile $pngfile &> /dev/null",
+        "temp_filename" => "latex_temp",
+        "output_directory" => "/latex",
+        "src_dir" => "",
+        "dst_dir" => ""
+      }
 
       @@generated_files = [ ]
       def self.generated_files
         @@generated_files
       end
 
-      @@latex_http_dir = ""
-      def self.latex_http_dir
-        @@latex_http_dir
+      def self.latex_output_directory
+        @@globals["output_directory"]
       end
 
       def initialize(tag_name, text, tokens)
         super
-        
-        # default values
-        @options = { 
-          "density" => @@default_latex_density, 
-          "usepackages" => ""
-        }
-        
         # We now can adquire the options for this liquid tag
+        @p = { "usepackages" => "" }
         text.gsub("  ", " ").split(" ").each do |part|
           if part.split("=").count != 2
             raise SyntaxError.new("Syntax Error in tag 'latex'")
           end
           var,val = part.split("=")
-          @options[var] = val
+          @p[var] = val
         end
       end
 
-      def self.read_config(site, param, default)
-        resp = site.config[param]
-        resp = default if resp.nil?
-        return resp
+      def self.read_config(name, site)
+        cfg = site.config["liquid_latex"]
+        return if cfg.nil?
+        value = cfg[name]
+        @@globals[name] = value if !value.nil?
       end
 
-      def self.init_configs(site)
+      def self.init_globals(site)
         # Get all the variables from the config and remember them for future use.
         if !defined?(@@first_time)
           @@first_time = true
-          @@debug_mode = read_config(site, "latex_debug_mode", false)
-          @@latex_cmd = read_config(site, "latex_latex_cmd", @@default_latex_latex_cmd)
-          @@dvips_cmd = read_config(site, "latex_dvips_cmd", @@default_latex_dvips_cmd)
-          @@convert_cmd = read_config(site, "latex_convert_cmd", @@default_latex_convert_cmd)
-          @@latex_temp_filename = read_config(site, "latex_temp_filename", @@default_latex_temp_filename)
-          @@latex_http_dir = read_config(site, "latex_output_directory", @@default_latex_output_directory)
-          @@latex_source_dir = File.join(site.config["source"], @@latex_http_dir)
-          @@latex_dest_dir = File.join(site.config["destination"] , @@latex_http_dir)
-          # Verify and prepare the output folder if it doesn't exists
-          FileUtils.mkdir_p(@@latex_source_dir) unless File.exists?(@@latex_source_dir)
+          @@globals.keys.each do |k|
+            read_config(k, site)
+          end
+          @@globals["src_dir"] = File.join(site.config["source"], @@globals["output_directory"])
+          @@globals["dst_dir"] = File.join(site.config["destination"], @@globals["output_directory"])
+          # Verify and prepare the output folder if it doesn't exist
+          FileUtils.mkdir_p(@@globals["src_dir"]) unless File.exists?(@@globals["src_dir"])
         end
       end
 
+      def execute_cmd(cmd)
+        cmd = cmd.gsub("\$density", @p["density"])
+        cmd = cmd.gsub("\$texfile", @p["tex_fn"])
+        cmd = cmd.gsub("\$dvifile", @p["dvi_fn"])
+        cmd = cmd.gsub("\$epsfile", @p["eps_fn"])
+        cmd = cmd.gsub("\$pngfile", @p["png_fn"])
+        puts cmd if @@globals["debug"]
+        system(cmd)
+        return ($?.exitstatus == 0)
+      end
+
       def render(context)
-        latex = super
-
+        latex_source = super
+        # fix initial configurations
         site = context.registers[:site]
-        Tags::LatexBlock::init_configs(site)
-
+        Tags::LatexBlock::init_globals(site)
+        # prepare density and usepackages
+        @p["density"] = @@globals["density"] unless @p.key?("density")
+        @p["usepackages"] = (@@globals["usepackages"].split(",") + @p["usepackages"].split(",")).join(",")
         # if this LaTeX code is already compiled, skip its compilation
-        hash_txt = @options["density"] + @options["usepackages"] + latex;
-        hash_txt = Digest::MD5.hexdigest(hash_txt)
-        filename = hash_txt + ".png"
-        filepath = File.join(@@latex_source_dir, filename)
-        if !File.exists?(filepath)
+        hash_txt = @p["density"] + @p["usepackages"] + latex_source
+        filename = Digest::MD5.hexdigest(hash_txt) + ".png"
+        @p["png_fn"] = File.join(@@globals["src_dir"], filename)
+        ok = true
+        if !File.exists?(@p["png_fn"])
+          puts "Compiling with LaTeX..." if @@globals["debug"]
+          @p["tex_fn"] = @@globals["temp_filename"] + ".tex"
+          @p["dvi_fn"] = @@globals["temp_filename"] + ".dvi"
+          @p["eps_fn"] = @@globals["temp_filename"] + ".eps"
 
-          # Use the options from the tag
-          density = @options["density"].to_i
-          raise SyntaxError.new("Invalid density value in tag 'latex'") if density <= 0
-          packages = ""
-          @options["usepackages"].gsub(" ","").split(",").each do |packagename|
-            packages << "\\usepackage\{#{packagename}\}\n"
+          # Put the LaTeX source code to file
+          latex_tex = "\\documentclass[letterpaper,dvips]{article}\n"
+          @p["usepackages"].gsub(" ","").split(",").each do |packagename|
+            latex_tex << "\\usepackage\{#{packagename}\}\n"
           end
-
-          # Put the LaTeX source code to a temporary file
-          latex_tex = "
-  \\documentclass[letterpaper,dvips]{article}
-  \\usepackage{pst-all}
-  \\usepackage{pst-3dplot}
-  [:PACKAGES:]
-  \\begin{document}
-  \\pagestyle{empty}
-  [:LATEX:]
-  \\end{document}"
-
-          latex_tex = latex_tex.gsub("[:PACKAGES:]", packages)
-          latex_tex = latex_tex.gsub("[:LATEX:]", latex)
-          tex_fn = @@latex_temp_filename + ".tex"
-          dvi_fn = @@latex_temp_filename + ".dvi"
-          eps_fn = @@latex_temp_filename + ".eps"
-          tex_file = File.new(tex_fn, "w")
+          latex_tex << "\\begin{document}\n\\pagestyle{empty}\n"
+          latex_tex << latex_source
+          latex_tex << "\\end{document}"
+          tex_file = File.new(@p["tex_fn"], "w")
           tex_file.puts(latex_tex)
           tex_file.close
-
           # Compile the document to PNG
-          cmd = @@latex_cmd.gsub("\$1", tex_fn)
-          puts cmd if @@debug_mode
-          system(cmd)
-          raise SyntaxError.new("Error compiling with latex in tag 'latex'") if $?.exitstatus != 0
-          cmd = @@dvips_cmd.gsub("\$1", dvi_fn).gsub("\$2", eps_fn)
-          puts cmd if @@debug_mode
-          system(cmd)
-          raise SyntaxError.new("Error compiling with dvips in tag 'latex'") if $?.exitstatus != 0
-          cmd = @@convert_cmd.gsub("\$1", eps_fn).gsub("\$2", filepath)
-          puts cmd if @@debug_mode
-          system(cmd)
-          raise SyntaxError.new("Error compiling with convert in tag 'latex'") if $?.exitstatus != 0
-
+          ok = execute_cmd(@@globals["latex_cmd"])
+          execute_cmd(@@globals["dvips_cmd"]) if ok
+          execute_cmd(@@globals["convert_cmd"]) if ok
           # Delete temporary files
-          Dir.glob(@@latex_temp_filename + ".*").each { |f| File.delete(f) }
-
+          Dir.glob(@@globals["temp_filename"] + ".*").each do |f|
+            File.delete(f)
+          end
         end
 
-        # Add the file to the list of static files for the final copy once generated
-        st_file = Jekyll::StaticFile.new(site, site.source, @@latex_http_dir, filename)
-        @@generated_files << st_file
-        site.static_files << st_file
-
-        # Build the <img> tag to return to the renderer
-        outpath = File.join(@@latex_http_dir, filename)
-        return "<img src=\"" + outpath + "\" />"
+        if ok
+          # Add the file to the list of static files for the final copy once generated
+          st_file = Jekyll::StaticFile.new(site, site.source, @@globals["output_directory"], filename)
+          @@generated_files << st_file
+          site.static_files << st_file
+          # Build the <img> tag to be returned to the renderer
+          png_path = File.join(@@globals["output_directory"], filename)
+          return "<img src=\"" + png_path + "\" />"
+        else
+          # Generate a block of text in the post with the original source
+          resp = "Failed to render the following block of LaTeX:<br/>\n"
+          resp << "<pre><code>" + latex_tex + "</code></pre>"
+          return resp
+        end
       end
     end
   end
@@ -143,8 +136,8 @@ module Jekyll
 
     def write
       super_write
-      Tags::LatexBlock::init_configs(self)
-      dest_folder = File.join(dest, Tags::LatexBlock::latex_http_dir)
+      Tags::LatexBlock::init_globals(self)
+      dest_folder = File.join(dest, Tags::LatexBlock::latex_output_directory)
       FileUtils.mkdir_p(dest_folder) unless File.exists?(dest_folder)
 
       # clean all previously rendered files not rendered in the actual build
@@ -152,12 +145,12 @@ module Jekyll
       Tags::LatexBlock::generated_files.each do |f|
         src_files << f.path
       end
-      pre_files = Dir.glob(File.join(source, Tags::LatexBlock::latex_http_dir, "*"))
+      pre_files = Dir.glob(File.join(source, Tags::LatexBlock::latex_output_directory, "*"))
       to_remove = pre_files - src_files
       to_remove.each do |f|
         File.unlink f if File.exists?(f)
         d, fn = File.split(f)
-        df = File.join(dest, Tags::LatexBlock::latex_http_dir, fn)
+        df = File.join(dest, Tags::LatexBlock::latex_output_directory, fn)
         File.unlink df if File.exists?(df)
       end
     end
